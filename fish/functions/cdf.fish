d7f871ad925c1d4d73ab126e2884181f7935ee3a
Antonio Morales <Antonio.Morales.-ND@disney.com>
2025-11-10 15:49:05 -0800

    update config

--- fish/functions/cdf.fish
+++ fish/functions/cdf.fish
@@ -1,6 +1,6 @@
-function vif -a query -d "search for a file and open it in vim"
+function cdf -a query -d "search for a file and cd to that directory"
     set --local file (fd --type f --hidden --color=always 2> /dev/null | fzf --query=$query --select-1 --exit-0 --ansi --preview='bat --style=numbers --color=always {}')
     if test -n "$file"
-        nvim $file
+        cd (path dirname $file)
     end
 end
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,11 +4,12 @@ local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smar
 local config = wezterm.config_builder()
 
 config.color_scheme = "OneNord"
-config.font = wezterm.font("CartographCF Nerd Font")
+config.font = wezterm.font("Cartograph CF")
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = false
 config.use_fancy_tab_bar = false
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.tab_max_width = 40
 config.inactive_pane_hsb = {
   saturation = 0.9,
   brightness = 0.6,
@@ -20,6 +21,7 @@ config.window_padding = {
   bottom = "0.5cell",
 }
 config.enable_scroll_bar = true
+config.scrollback_lines = 10000
 
 config.leader = { key = "o", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
@@ -27,6 +29,11 @@ config.keys = {
     key = "|",
     mods = "LEADER|SHIFT",
     action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+  },
+  {
+    key = "?",
+    mods = "LEADER",
+    action = wezterm.action.ShowDebugOverlay,
   },
   {
     key = "-",
@@ -76,6 +83,21 @@ config.keys = {
     mods = "LEADER",
     action = wezterm.action.RotatePanes("Clockwise"),
   },
+  {
+    key = "w",
+    mods = "LEADER",
+    action = wezterm.action.CloseCurrentPane({ confirm = true }),
+  },
+  {
+    key = "h",
+    mods = "LEADER",
+    action = wezterm.action.MoveTabRelative(-1),
+  },
+  {
+    key = "l",
+    mods = "LEADER",
+    action = wezterm.action.MoveTabRelative(1),
+  },
 }
 
 smart_splits.apply_to_config(config, {
@@ -91,11 +113,18 @@ local function basename(s)
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local working_dir = basename(tab.active_pane.current_working_dir.file_path)
+  wezterm.log_warn("working dir: " .. working_dir)
   local process_name = basename(tab.active_pane.foreground_process_name)
   local process_icon = process_name
-  local title = (tab.tab_index + 1) .. ": " .. process_icon .. " "
 
-  return title
+  if tab.is_active then
+    local title = (tab.tab_index + 1) .. ": " .. working_dir .. " - " .. process_icon .. " "
+    return title
+  else
+    local title = (tab.tab_index + 1) .. ": " .. working_dir .. " "
+    return title
+  end
 end)
 
 return config
